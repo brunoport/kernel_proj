6568c9bff255c0b2cefc0131265286c5fc1f0fc0
brunoport <b-portugal@hotmail.com>
2017-07-06 01:25:29 -0300

    agora com multiplos testes e README

--- ex-mycall.c
+++ ex-mycall.c
@@ -9,8 +9,22 @@
 int main() {
     int n1 = 50;
     int n2 = 5;
+
     char o = '+';
     int r = syscall(384,n1,n2,o);
-    printf("-------\nex-mycall: \nChamei com %d, %d, %c \nRetorno da chamada de sistema: %d.\n", n1, n2, o, r);
+    printf("-------\nex-mycall: (%d, %d, %c) \nRetorno da chamada de sistema: %d.\n", n1, n2, o, r);
+
+    o = '-';
+    r = syscall(384,n1,n2,o);
+    printf("-------\nex-mycall: (%d, %d, %c) \nRetorno da chamada de sistema: %d.\n", n1, n2, o, r);
+
+    o = '*';
+    r = syscall(384,n1,n2,o);
+    printf("-------\nex-mycall: (%d, %d, %c) \nRetorno da chamada de sistema: %d.\n", n1, n2, o, r);
+
+    o = '/';
+    r = syscall(384,n1,n2,o);
+    printf("-------\nex-mycall: (%d, %d, %c) \nRetorno da chamada de sistema: %d.\n", n1, n2, o, r);
+    
     return 0;
 }
